1208a75619a63ddaff4060c708e26af1e425c1f8
Christian <christian.auman@outlook.com>
2021-09-07 17:29:50 -0400

    Update

--- Portfolio Website/my-portfolio/src/App.js
+++ Portfolio Website/my-portfolio/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';  
+import React, { Fragment, useEffect, useState } from 'react';  
 import $ from 'jquery'; 
 import * as Scroll from 'react-scroll';
 import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
 import Carousel from './modules/carousel';
-import Card from './modules/card';
+import Card from './modules/card'; 
 const date = new Date();
 let i = 0;  
 let a = 1100;  
+const UnityPooperPunk = () => {
+  return(
+    <Fragment>hello there!</Fragment>
+  );
+}
 function ForwardsLoop(word, text, setText) {
   setTimeout(() => {  
     setText(word.substring(0, i));
@@ -74,17 +79,22 @@ function App() {
             />,
             <Card rf='https://www.cameraegg.org/wp-content/uploads/2016/01/Nikon-D500-Sample-Images-2.jpg' 
               title='Number Wizard'
+              description='Number Wizard is a spin off of a very basic game that most programmers learn to make. It will eventually guess the number that you have in your head using basic math and logic.'
             />,
             <Card rf='https://www.cameraegg.org/wp-content/uploads/2016/01/Nikon-D500-Sample-Images-2.jpg' 
               title='Block Breaker'
+              description="This game was one of the first BIG games that i've made. This was the game that taught me the most about unity's 2D engine."
+            />,
+            <Card rf='https://www.cameraegg.org/wp-content/uploads/2016/01/Nikon-D500-Sample-Images-2.jpg'
+              title='2D Platformer'
+              description="This game doesn't have a name because I never fully completed it. This game was the first time I tried to use tilemaps and some other features that I had to learn."
             />,
           ]}/>
         </div>
       </div>
       <div id='web'>
         <h1 id='title'>Web</h1>
-        <section>
-          <p>SAMPLE TEXT</p>
+        <section>  
           <p>SAMPLE TEXT</p>
           <p>SAMPLE TEXT</p>
           <p>SAMPLE TEXT</p>
@@ -117,7 +127,7 @@ function App() {
           <p>SAMPLE TEXT</p>
         </section>
       </div>
-    </div>
+    </div> 
   );
 }
 
--- Portfolio Website/my-portfolio/src/modules/carousel.js
+++ Portfolio Website/my-portfolio/src/modules/carousel.js
@@ -2,33 +2,63 @@ import React, {useState} from 'react';
 import Card from './card';
 import ArrowButton from './arrowButton';
 import './carousel.css';
+import $ from 'jquery';
 
 
 const Carousel = (props) => {
     const [cardList, setCardList] = useState(props.cards);
-    const shiftListRight = () => {
+    const shiftListLeft = () => {
         const tempList = [];
         for (var i = 0; i < cardList.length - 1; i++) {
             tempList.push(cardList[i]); 
         }
-        tempList.unshift(cardList[cardList.length - 1]);  
+        tempList.unshift(cardList[cardList.length - 1]);   
+        setCardList(tempList);   
         setCardList(tempList);
+        $('#card-1').css({transform: 'translateY(0px)'});
+        $('#card-3').css({transform: 'translateY(0px)'}); 
+        $('#card-1').css({filter: 'blur(0)'});
+        $('#card-3').css({filter: 'blur(0)'});
+        setTimeout(() => { 
+            $('#card-3').css({transform: 'translateY(10px)'});
+            $('#card-1').css({transform: 'translateY(10px)'});
+            $('#card-1').css({filter: 'blur(1px)'});
+            $('#card-3').css({filter: 'blur(1px)'});
+        }, 250);
     }
 
-    const shiftListLeft = () => {
+    const shiftListRight = () => {
         const tempList = [];
         for (var i = 1; i < cardList.length; i++) {
             tempList.push(cardList[i]); 
         }
         tempList.push(cardList[0]);  
         setCardList(tempList);
+        $('#card-1').css({transform: 'translateY(0px)'});
+        $('#card-3').css({transform: 'translateY(0px)'});
+        $('#card-1').css({filter: 'blur(0)'});
+        $('#card-3').css({filter: 'blur(0)'});
+        setTimeout(() => { 
+            $('#card-3').css({transform: 'translateY(10px)'});
+            $('#card-1').css({transform: 'translateY(10px)'});
+            $('#card-1').css({filter: 'blur(1px)'});
+            $('#card-3').css({filter: 'blur(1px)'});
+        }, 250);
     }
     return(
         <div id='carousel'> 
             <span onClick={shiftListLeft}><ArrowButton symbol='◀' /></span>
-            {cardList.map((card, index) => ( 
-                index>2 ? <span></span> : card
-            ))}
+            {cardList.map((card, index) => { 
+                if (index > (window.outerWidth > 1127 ? 2 : 0)) {
+                    return(<span></span>);
+                } else if (window.outerWidth <= 1127) { 
+                    return(<div id={'card-2'}>{card}</div>);
+                }else if (index == 1) {
+                    return(<div id={'card-' + (index + 1)}>{card}</div>);
+                } else {
+                    return(<div id={'card-' + (index + 1)}>{card}</div>);
+                } 
+            })}
             <span onClick={shiftListRight}><ArrowButton symbol='▶' /></span>
         </div>
     );
