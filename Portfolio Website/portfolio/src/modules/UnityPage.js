3aaee74ac75dd137cd0f34e3abe54b271edaf098
Christian <christian.auman@outlook.com>
2021-09-13 14:29:44 -0400

    update

--- Portfolio Website/portfolio/src/modules/UnityPage.js
+++ Portfolio Website/portfolio/src/modules/UnityPage.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import styles from './UnityPage.module.css'; 
-
+import React, { useEffect } from 'react';
+import styles from './UnityPage.module.css';  
+import $ from 'jquery';
+ 
 const UnityPage = (props) => {
+    useEffect(() => {
+        $(window).scrollTop(0);
+        $('html').css({background: '#212529'});
+    }, []);
     return(
         <div id={styles.page}>
             <div id={styles.leftSide}>  
                 <div className={styles.titleBackground}> 
-                    <h1 className={styles.title}>{props.title}</h1> 
+                    <h1 className={styles.title}>{props.title}</h1>  
                 </div>
                 <img src={props.src}></img>
             </div>
             <div>
-                <p className={styles.description}>Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text.</p>
+                <p className={styles.description}>{props.description}</p>
             </div> 
         </div>
     );
