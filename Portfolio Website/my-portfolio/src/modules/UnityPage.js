b3a1897ff7500689763d0d8544fda24e54c0bd16
Christian <christian.auman@outlook.com>
2021-09-15 11:34:52 -0400

    update

--- Portfolio Website/my-portfolio/src/modules/UnityPage.js
+++ Portfolio Website/my-portfolio/src/modules/UnityPage.js
@@ -11,6 +11,7 @@ const UnityPage = (props) => {
     } 
     useEffect(() => {
         $(window).scrollTop(0); 
+        $('html').css({background: '#212529'});
         window.addEventListener('scroll', handleScroll);
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
@@ -18,12 +19,12 @@ const UnityPage = (props) => {
         <div id={styles.page}> 
             <Link to='' id={styles.back}><ArrowButton symbol='◀ Home'/></Link> 
              <div id={styles.leftSide}>  
-                <div className={styles.titleBackground}>  
+                <div className={styles.titleBackground} >  
                     <h1 className={styles.title} style={{transform: `skewY(${offsetY * 0.01}deg)`, opacity: `${Math.cos(offsetY * 0.005)}`}}>{props.title}</h1>  
                 </div>
                 <img src={props.src} style={{transform: `translateY(${offsetY * 0.6}px)`}}></img>
             </div> 
-            <div id={styles.descriptionBackground}>
+            <div id={styles.descriptionBackground}> 
                 <h1 id={styles.descriptionTitle}>About</h1>
                 <p className={styles.description}>{props.description}</p>
             </div> 
