8c45a765ff0985cd93dd43cf8a5019f895549e80
Christian <christian.auman@outlook.com>
2021-09-16 17:09:21 -0400

    Update

--- Portfolio Website/my-portfolio/src/modules/web.js
+++ Portfolio Website/my-portfolio/src/modules/web.js
@@ -8,7 +8,8 @@ const Web = () => {
     
     const [offsetY, setOffsetY] = useState(window.pageYOffset);
     const handleScroll = () => {
-        setOffsetY(window.pageYOffset);
+        setOffsetY(window.scrollY);
+        console.log(window.scrollY);
     };
     useEffect(() => {
         window.addEventListener('scroll', handleScroll);
@@ -21,40 +22,40 @@ const Web = () => {
                 <div id='outer-flex'>
                     <h1 id='title'>Web Development</h1> 
                     <div id='flex'>
-                        <div style={{transform: `translateX(${(offsetY * 0.09 - 170) >= 0 ? 0 : (offsetY * 0.09 - 170)}px)`, opacity: `${offsetY * 0.0048 - 8}`}}>
+                        <div style={{transform: `translateX(${(offsetY * 0.09 - 170) >= 0 || (offsetY - 1504) < 0 ? 0 : (offsetY * 0.09 - 170)}px)`, opacity: `${offsetY * 0.0048 - 8}`}}>
                             <h1 className='title'>Html</h1>
                             <span className='underline' /> 
                             <p>Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text.</p> 
                         </div>
-                        <div style={{transform: `translateY(${(offsetY * 0.09 - 170) >= 0 ? 0 : (offsetY * 0.09 - 170)}px)`, opacity: `${offsetY * 0.0048 - 8}`}}>
+                        <div style={{transform: `translateY(${(offsetY * 0.09 - 170) >= 0 || (offsetY - 1504) < 0 ? 0 : (offsetY * 0.09 - 170)}px)`, opacity: `${offsetY * 0.0048 - 8}`}}>
                             <h1 className='title'>Css</h1>
                             <span className='underline' />
                             <p>Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text.</p>
                         </div>
-                        <div style={{transform: `translateX(${-(offsetY * 0.09 - 170) <= 0 ? 0 : -(offsetY * 0.09 - 170)}px)`, opacity: `${offsetY * 0.0048 - 8}`}}>
+                        <div style={{transform: `translateX(${-(offsetY * 0.09 - 170) <= 0 || (offsetY - 1504) < 0 ? 0 : -(offsetY * 0.09 - 170)}px)`, opacity: `${offsetY * 0.0048 - 8}`}}>
                             <h1 className='title'>Javascript</h1>
                             <span className='underline' />
                             <p>Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text.</p>
                         </div>  
                     </div>
                 </div> 
-            <img id='transitionImg' />
+                <img id='transitionImg' />
+                <img  src='https://logos-download.com/wp-content/uploads/2016/09/React_logo_logotype_emblem.png' width='400px' className='reactLogo' style={{transform: `translateX(${(offsetY - 2300) < offsetY ? (offsetY - 2300) * 0.5 - 450 : 0}px) rotate(${offsetY * 0.1}deg)`}}/>
             </div> 
             <div id='react'>
-                <img  src='https://logos-download.com/wp-content/uploads/2016/09/React_logo_logotype_emblem.png' width='400px' className='reactLogo' style={{transform: `translateX(${offsetY * 0.2 -750}px) rotate(${offsetY * 0.1}deg)`}}/>
                 <h1 id='title'>React</h1>
                     <div id='flex2'>
-                        <div style={{transform: `translateX(${(offsetY * 0.09 - 300) >= 0 ? 0 : (offsetY * 0.09 - 300)}px)`, opacity: `${offsetY * 0.0048 - 14.87}`}}>
+                        <div style={{transform: `translateX(${(offsetY * 0.09 - 300) >= 0 || (offsetY -2960) < 0 ? 0 : (offsetY * 0.09 - 300)}px)`, opacity: `${offsetY * 0.0048 - 14.87}`}}> 
                             <h1 className='title'>Html</h1>
                             <span className='underline' /> 
                             <p>Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text.</p> 
                         </div>
-                        <div style={{transform: `translateY(${(offsetY * 0.09 - 300) >= 0 ? 0 : (offsetY * 0.09 - 300)}px)`, opacity: `${offsetY * 0.0048 - 14.87}`}}>
+                        <div style={{transform: `translateY(${(offsetY * 0.09 - 300) >= 0 || (offsetY -2960) < 0 ? 0 : (offsetY * 0.09 - 300)}px)`, opacity: `${offsetY * 0.0048 - 14.87}`}}>
                             <h1 className='title'>Css</h1>
                             <span className='underline' />
                             <p>Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text.</p>
                         </div>
-                        <div style={{transform: `translateX(${-(offsetY * 0.09 - 300) <= 0 ? 0 : -(offsetY * 0.09 - 300)}px)`, opacity: `${offsetY * 0.0048 - 14.87}`}}>
+                        <div style={{transform: `translateX(${-(offsetY * 0.09 - 300) <= 0 || offsetY -2960 < 0 ? 0 : -(offsetY * 0.09 - 300)}px)`, opacity: `${offsetY * 0.0048 - 14.87}`}}>
                             <h1 className='title'>Javascript</h1>
                             <span className='underline' />
                             <p>Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text.</p>
