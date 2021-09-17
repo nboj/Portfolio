9b0335326a4c8acaf80cb6d99cbd28ef8e65397a
Christian <christian.auman@outlook.com>
2021-09-16 22:20:08 -0400

    Update

--- Portfolio Website/my-portfolio/src/App.js
+++ Portfolio Website/my-portfolio/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, Router } from 'react-router-dom';  
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';  
 import Home from './pages/Home'; 
 import UnityLinks, { NumberWizard, PooperPunk, BlockBreaker, Platformer } from './pages/UnityLinks';
 import $ from 'jquery'; 
@@ -7,23 +7,25 @@ import $ from 'jquery';
 const App = () => { 
   return (  
     <Router>
-      <Switch>
-        <Route exact path='/'> 
-          <Home />
-        </Route>
-        <Route path='/NumberWizard'>
-          <NumberWizard />
-        </Route>
-        <Route path='/PooperPunk'>
-          <PooperPunk />
-        </Route>
-        <Route path='/BlockBreaker'>
-          <BlockBreaker />
-        </Route>
-        <Route path='/Platformer'>
-          <Platformer />
-        </Route>
-      </Switch> 
+      <div>
+        <Switch>
+          <Route path='/' exact> 
+            <Home />
+          </Route>
+          <Route path='/NumberWizard'>
+            <NumberWizard />
+          </Route>
+          <Route path='/PooperPunk'>
+            <PooperPunk />
+          </Route>
+          <Route path='/BlockBreaker'>
+            <BlockBreaker />
+          </Route>
+          <Route path='/Platformer'>
+            <Platformer />
+          </Route>
+        </Switch> 
+      </div>
     </Router>
   );
 }
