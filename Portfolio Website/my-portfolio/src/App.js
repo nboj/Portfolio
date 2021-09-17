a8f88a067d4144d16f8ca82469706a3b83e4ea97
Christian <christian.auman@outlook.com>
2021-09-16 22:12:58 -0400

    Update App.js

--- Portfolio Website/my-portfolio/src/App.js
+++ Portfolio Website/my-portfolio/src/App.js
@@ -1,28 +1,30 @@
 import React from 'react';
-import { Route, Switch} from 'react-router-dom';  
+import { Route, Switch, Router } from 'react-router-dom';  
 import Home from './pages/Home'; 
 import UnityLinks, { NumberWizard, PooperPunk, BlockBreaker, Platformer } from './pages/UnityLinks';
 import $ from 'jquery'; 
 
 const App = () => { 
   return (  
-    <Switch>
-      <Route exact path='/'> 
-        <Home />
-      </Route>
-      <Route path='/NumberWizard'>
-        <NumberWizard />
-      </Route>
-      <Route path='/PooperPunk'>
-        <PooperPunk />
-      </Route>
-      <Route path='/BlockBreaker'>
-        <BlockBreaker />
-      </Route>
-      <Route path='/Platformer'>
-        <Platformer />
-      </Route>
-    </Switch> 
+    <Router>
+      <Switch>
+        <Route exact path='/'> 
+          <Home />
+        </Route>
+        <Route path='/NumberWizard'>
+          <NumberWizard />
+        </Route>
+        <Route path='/PooperPunk'>
+          <PooperPunk />
+        </Route>
+        <Route path='/BlockBreaker'>
+          <BlockBreaker />
+        </Route>
+        <Route path='/Platformer'>
+          <Platformer />
+        </Route>
+      </Switch> 
+    </Router>
   );
 }
 
