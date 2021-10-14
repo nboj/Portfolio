09d0a654b0520608ce51a90e37ac27d4d50975f8
Christian <christian.auman@outlook.com>
2021-10-14 14:21:24 -0400

    Update

--- Portfolio Website/my-portfolio/src/modules/UnityPage.js
+++ Portfolio Website/my-portfolio/src/modules/UnityPage.js
@@ -1,10 +1,9 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import styles from './UnityPage.module.css';
 import $ from 'jquery';
 import ArrowButton from './arrowButton'; 
 import { Link } from 'react-router-dom';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
-import { config } from '@react-spring/web';
 
 const UnityPage = (props) => {
     useEffect(() => {
@@ -12,21 +11,36 @@ const UnityPage = (props) => {
     }, []);
     return(
         <div id={styles.page}>
-                    <Link to='' id={styles.back}><ArrowButton symbol='◀ Home'/></Link>
+        <Link to='' id={styles.back}><ArrowButton symbol='◀ Home'/></Link>
         <Parallax
             pages={2}
+            config={{
+                mass: 0.1,
+                tension: 500,
+                friction: 1,
+                clamp: true
+            }}
         >
-                 <div id={styles.leftSide}>
-                    <div className={styles.titleBackground} >
-                    <ParallaxLayer offset={0} speed={0.4} style={{zIndex: 10}}>
-                        <h1 className={styles.title}>{props.title}</h1>
-                    </ParallaxLayer>
-                    <ParallaxLayer offset={0} speed={-0.1} config={config.molasses}>
-                        <div style={{ backgroundImage: `url(${props.src})`, backgroundSize: 'cover', backgroundPosition: 'center'}}/>
-                    </ParallaxLayer>
+                <ParallaxLayer
+                    offset={0}
+                >
+                     <div id={styles.leftSide}>
+                        <div className={styles.titleBackground} >
+                            <ParallaxLayer offset={0} speed={0.4} style={{zIndex: 10}}>
+                                <h1 className={styles.title}>{props.title}</h1>
+                            </ParallaxLayer>
+                            <ParallaxLayer
+                                offset={0}
+                                speed={-0.3}
+                            >
+                                <div style={{ backgroundImage: `url(${props.src})`, backgroundSize: 'cover', backgroundPosition: 'center'}}/>
+                            </ParallaxLayer>
+                        </div>
                     </div>
-                </div>
-                <ParallaxLayer offset={1}>
+                </ParallaxLayer>
+                <ParallaxLayer
+                    offset={1}
+                >
                     <div id={styles.descriptionBackground}>
                         <h1 id={styles.descriptionTitle}>About</h1>
                         <p className={styles.description}>{props.description}</p>
@@ -34,7 +48,7 @@ const UnityPage = (props) => {
                 </ParallaxLayer>
         </Parallax>
         </div>
-    );
+    )
 }
 
 export default UnityPage;
